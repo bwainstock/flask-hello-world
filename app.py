e946424cc08cb3ca3e9b2d4af66235bf55157f3e
Barry Wainstock <barrywainstock@gmail.com>
2016-10-01 15:58:59 -0700

    Sort headers

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 def index():
     time = datetime.datetime.now().isoformat()
     hostname = socket.gethostname()
-    headers = "<br>".join(["<b>{}</b>: {}".format(x,y) for x,y in request.headers])
+    sorted_headers = sorted(request.headers)
+    headers = "<br>".join(["<b>{}</b>: {}".format(x,y) for x,y in sorted_headers])
     return "<h1>{}</h1><h3>{}</h3><br>{}".format(hostname, time, headers)
 
 if __name__ == "__main__":
